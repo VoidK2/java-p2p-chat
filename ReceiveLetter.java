af85410c40b4bd7ca06db5741d2f3121f658f040
VoidK2 <1399477504@qq.com>
2017-12-29 13:59:53 +0800

    Update ReceiveLetter.java

--- ReceiveLetter.java
+++ ReceiveLetter.java
@@ -1,31 +1,31 @@
-import javax.sound.midi.Receiver;
 import java.net.*;
-import java.util.*;
-public class ComputerA {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        Thread readData;
-        ReceiveLetter rec = new ReceiveLetter(10029);
-        try{readData= new Thread(rec);
-            readData.start();
-            byte [] buffer = new byte[1];
-            InetAddress address = InetAddress.getByName("127.0.0.1");
-            DatagramPacket datapack = new DatagramPacket(buffer,buffer.length,address,10024);
-            DatagramSocket postman  = new DatagramSocket();
-            System.out.println("input the message to computer a");
-            while(in.hasNext()){
-                String mess=in.nextLine();
-                buffer = mess.getBytes();
-                if(mess.length()==0){
-                    System.exit(0);
+public class ReceiveLetter implements Runnable{
+    int port;
+    ReceiveLetter(int port){
+        this.port=port;
+    }
+    public void run(){
+        DatagramPacket pack = null;
+        DatagramSocket postman=null;
+        byte data[] = new byte[8192];
+        try{
+            pack=new DatagramPacket(data,data.length);
+            postman = new DatagramSocket(port);
+        }
+        catch(Exception e){
+            System.out.println(" Computer error");
+        }
+        while(true){
+            if(postman==null)
+                break;
+            else
+                try{postman.receive(pack);
+                    String message = new String(pack.getData(),0,pack.getLength());
+                    System.out.printf("%25s\n","收到"+message);
+                }catch (Exception e){
+                    System.out.println("rl error");
                 }
-                buffer = mess.getBytes();
-                datapack.setData(buffer);
-                postman.send(datapack);
- //              System.out.println("input continue");
-            }
-        }catch (Exception e){
-            System.out.println(e);
         }
     }
+
 }
